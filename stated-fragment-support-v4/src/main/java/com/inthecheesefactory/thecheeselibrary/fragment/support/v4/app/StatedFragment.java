b7305ca281ede91051edbc5e437bf7610cf77ae5
nuuneoi <sittiphol@gmail.com>
2015-03-13 15:52:16 +0700

    0.9.2 (Add onActivityResult for Nested Fragment)

--- stated-fragment-support-v4/src/main/java/com/inthecheesefactory/thecheeselibrary/fragment/support/v4/app/StatedFragment.java
+++ stated-fragment-support-v4/src/main/java/com/inthecheesefactory/thecheeselibrary/fragment/support/v4/app/StatedFragment.java
@@ -1,7 +1,12 @@
 package com.inthecheesefactory.thecheeselibrary.fragment.support.v4.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
+import com.inthecheesefactory.thecheeselibrary.fragment.support.v4.app.bus.ActivityResultBus;
+import com.inthecheesefactory.thecheeselibrary.fragment.support.v4.app.bus.ActivityResultEvent;
+import com.squareup.otto.Subscribe;
 
 /**
  * Created by nuuneoi on 11/16/2014.
@@ -135,4 +140,27 @@ public class StatedFragment extends Fragment {
     protected void onSaveState(Bundle outState) {
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        ActivityResultBus.getInstance().register(mActivityResultSubscriber);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ActivityResultBus.getInstance().unregister(mActivityResultSubscriber);
+    }
+
+    private Object mActivityResultSubscriber = new Object() {
+        @Subscribe
+        public void onActivityResultReceived(ActivityResultEvent event) {
+            int requestCode = event.getRequestCode();
+            int resultCode = event.getResultCode();
+            Intent data = event.getData();
+            onActivityResult(requestCode, resultCode, data);
+        }
+    };
+
 }
--- stated-fragment/src/main/java/com/inthecheesefactory/thecheeselibrary/fragment/StatedFragment.java
+++ stated-fragment/src/main/java/com/inthecheesefactory/thecheeselibrary/fragment/StatedFragment.java
@@ -1,7 +1,12 @@
 package com.inthecheesefactory.thecheeselibrary.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.inthecheesefactory.thecheeselibrary.fragment.bus.ActivityResultBus;
+import com.inthecheesefactory.thecheeselibrary.fragment.bus.ActivityResultEvent;
+import com.squareup.otto.Subscribe;
 
 /**
  * Created by nuuneoi on 11/16/2014.
@@ -135,4 +140,26 @@ public class StatedFragment extends Fragment {
     protected void onSaveState(Bundle outState) {
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        ActivityResultBus.getInstance().register(mActivityResultSubscriber);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ActivityResultBus.getInstance().unregister(mActivityResultSubscriber);
+    }
+
+    private Object mActivityResultSubscriber = new Object() {
+        @Subscribe
+        public void onActivityResultReceived(ActivityResultEvent event) {
+            int requestCode = event.getRequestCode();
+            int resultCode = event.getResultCode();
+            Intent data = event.getData();
+            onActivityResult(requestCode, resultCode, data);
+        }
+    };
 }
